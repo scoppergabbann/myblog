a09608596f69b5f5c678d15e2e14799d43b19e08
Scopper Gabban <mcfawwaz@hotmail.com>
2026-05-16 09:16:07 +0700

    remove obsolete mdx-renderer (uses removed next-mdx-remote)

--- components/mdx-renderer.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { MDXRemote, type MDXRemoteSerializeResult } from 'next-mdx-remote';
-import { mdxComponents } from './mdx-components';
-
-export function MdxRenderer({ source }: { source: MDXRemoteSerializeResult }) {
-  return <MDXRemote {...source} components={mdxComponents} />;
-}
